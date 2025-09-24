765a3a28b14e97ceaa407f466c05f4add13743f4
Stewart Adam <s.adam@diffingo.com>
2025-09-24 02:23:30 -0700

    Update to Python 3.13

--- regen-deps.sh
+++ regen-deps.sh
@@ -6,4 +6,4 @@ uv export --frozen --no-dev --no-emit-workspace --format requirements.txt --no-h
 # do not use flatpak-pip-generator; it cannot download binary wheels which is required for cryptograph
 # https://github.com/johannesjh/req2flatpak?tab=readme-ov-file#related-work
 python3 -m pip install -U --user git+https://github.com/johannesjh/req2flatpak
-req2flatpak --requirements-file requirements.txt --target-platforms 311-x86_64 311-aarch64 >python3-modules.json
+req2flatpak --requirements-file requirements.txt --target-platforms 313-{x86_64,aarch64} >python3-modules.json
