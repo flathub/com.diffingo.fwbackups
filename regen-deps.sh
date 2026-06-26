553b631cb412703d46b643fc59e0c2dd4750f78e
Sabri Ünal <yakushabb@gmail.com>
2026-06-26 20:51:34 +0300

    Update the runtime version from 49 to 50, and more
    - Update the runtime version to 50
    - Drop ineffective cleanup commands
    - Create a requirements.txt file
    - Regenerate Python modules
    - Comply with Flathub styling guidelines
    
    See: https://docs.flathub.org/docs/for-app-authors/requirements#flatpak-builder-manifest-style-guide
    
    Signed-off-by: Sabri Ünal <yakushabb@gmail.com>

--- regen-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python3 -m pip install -U --user uv
-uv export --frozen --no-dev --no-emit-workspace --format requirements.txt --no-hashes -o requirements.txt --project ../fwbackups
-
-# do not use flatpak-pip-generator; it cannot download binary wheels which is required for cryptograph
-# https://github.com/johannesjh/req2flatpak?tab=readme-ov-file#related-work
-python3 -m pip install -U --user git+https://github.com/johannesjh/req2flatpak
-req2flatpak --requirements-file requirements.txt --target-platforms 313-{x86_64,aarch64} >python3-modules.json
